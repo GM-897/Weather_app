3747b796b77bdbf2c88fa9e33701ce556a381d56
GM-897 <97469161+GM-897@users.noreply.github.com>
2023-03-16 00:34:45 +0530

    few changes

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -5,33 +5,33 @@ const forecast=({data})=>{
     return(
         <div className="forecast">
             <div className="today">
-        <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon" class="w-icon" />
+        <img src ={`https://openweathermap.org/img/w/${data && data.list[0].weather[0].icon}.png`} alt="icon" className="w-icon" />
              <div className="contain">
-                <div className="day">SOMEDAY</div>   
-                <div className="temp">27.3</div>
+                <div className="day">{data.list[0].dt}</div>   
+                <div className="temp">{data.list[0].main.temp}</div>
             </div>
                 
             </div>
             <div className="other-days">
                 <div className="days">
                     <div className="day">TUE</div>
-                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon" class="w-icon" />
-                    <div className="temp">33.4</div>
+                    <img src={`https://openweathermap.org/img/w/${data && data.list[8].weather[0].icon}.png`} alt="icon" className="w-icon" />
+                    <div className="temp">{data.list[8].main.temp}</div>
                 </div>
                 <div className="days">
                     <div className="day">TUE</div>
-                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon" class="w-icon" />
-                    <div className="temp">33.4</div>
+                    <img src={`https://openweathermap.org/img/w/${data && data.list[16].weather[0].icon}.png`} alt="icon" className="w-icon" />
+                    <div className="temp">{data.list[16].main.temp}</div>
                 </div>
                 <div className="days">
                     <div className="day">TUE</div>
-                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon" class="w-icon" />
-                    <div className="temp">33.4</div>
+                    <img src={`https://openweathermap.org/img/w/${data && data.list[24].weather[0].icon}.png`} alt="icon" className="w-icon" />
+                    <div className="temp">{data.list[24].main.temp}</div>
                 </div>
                 <div className="days">
                     <div className="day">TUE</div>
-                    <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon" class="w-icon" />
-                    <div className="temp">33.4</div>
+                    <img src={`https://openweathermap.org/img/w/${data && data.list[32].weather[0].icon}.png`} alt="icon" className="w-icon" />
+                    <div className="temp">{data.list[32].main.temp}</div>
                 </div>
             </div>
         </div>
